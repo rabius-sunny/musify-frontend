1fc9de52ff106d3db9a4432724ef1f5d95f0bf78
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-19 02:05:02 +0600

    endpoint

--- src/utils/endpoint.ts
+++ src/utils/endpoint.ts
@@ -1,5 +1,5 @@
 const endpoint =
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3001/api'
-    : 'http://localhost:3001/api'
+    ? 'http://localhost:3000/api'
+    : 'https://www.darulislam.net/api'
 export default endpoint
